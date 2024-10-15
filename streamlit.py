b78ca3131d964da139472fd923d77f6e14d4dbd5
HarleyChen <42833195+MrHarBear@users.noreply.github.com>
2024-10-15 20:04:07 +0100

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,29 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+import streamlit.components.v1 as components
+
+# Write directly to the app
+st.title("Drift Detection Reports")
+
+# Get the current credentials
+session = get_active_session()
+
+# Get available reports
+files = session.sql('LS @CONTAINER_RUNTIME_LAB.NOTEBOOKS.MONITORING').collect()
+files = sorted(
+    [file['name'] for file in files if file['name'].endswith('.html')],
+    reverse=True
+)
+report_selection = st.selectbox('Select Report File:', files)
+
+@st.cache_data
+def get_report():
+    file = session.file.get_stream(f'@CONTAINER_RUNTIME_LAB.NOTEBOOKS.{report_selection}').read()
+    
+    # Convert bytes to string
+    html_content = file.decode('utf-8')
+    return html_content
+
+report = get_report()
+components.html(report, width=1000, height=1200, scrolling=True)
